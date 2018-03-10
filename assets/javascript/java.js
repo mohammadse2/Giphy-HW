33828ec3c9379bc699b2aa77ac2b6223c309a8db
unknown <mpowerelectronic@gmail.com>
2018-03-09 20:19:33 -0800

    Create file

---
+++ assets/javascript/java.js
@@ -0,0 +1,91 @@
+$("body").css({ "background-image": "url('assets/images/background.jpg')","height":"100%","background-size":"cover"})
+
+$(document).ready(function () {
+  // Array of animal 
+  var animals = ["lion", "tiger", "snake", "bee", "bear", "dog", "wolf", "cat", "cow", "duck"]
+
+
+  // Display animal in the button
+  function animalButtons() {
+    $("#buttons-area").empty();
+    for (var i = 0; i < animals.length; i++) {
+      var button = $("<button>");
+      button.addClass("btnClass");
+      button.attr("buttonName", animals[i]);
+      button.text(animals[i]);
+      $("#buttons-area").append(button);
+    }
+  }  // End of the animalButtons function
+
+
+
+  // Add new item to the item list
+  $("#add-animal").on("click", function (event) {
+    event.preventDefault(); // ++mySelf++
+    var animal = $("#animal-input").val().trim();
+    if (animal === "") {
+      alert("You left the link empty, Please enter an animal name then push the 'Add an animal' button")
+      return false;
+    }
+    animals.push(animal);
+    $("#animal-input").val("") // ++mySelf++
+    animalButtons();
+  });
+
+
+  function displayAnimalInfo() {
+    var animal = $(this).attr("buttonName")
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=Nf6AC5rFzRhteVH5i5N1a5EzGYqxwlt3"
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      $("#animal-view").empty();
+
+
+
+      for (var j = 0; j < response.data.length; j++) {
+
+        var imageDiv = $("<div>");
+        imageDiv.addClass("imgClass");
+
+        var pOne = $("<p>").text("Rating: " + response.data[j].rating);
+        imageDiv.append(pOne);
+
+        var image = $("<img>");
+        image.attr("src", response.data[j].images.fixed_height_small_still.url);
+        image.attr("data-still", response.data[j].images.fixed_height_small_still.url);
+        image.attr("data-animate", response.data[j].images.fixed_height_small.url);
+        image.attr("data-state", "still");
+        image.addClass("imgClass");
+        imageDiv.append(image);
+        $("#animal-view").prepend(imageDiv);
+
+      }
+
+    });  // End of the response function
+
+  }   // End of the displayAnimalInfo function
+
+
+
+  $(document).on("click", ".btnClass", displayAnimalInfo);
+  $(document).on("click", ".imgClass", function () {
+    var state = $(this).attr('data-state');
+
+    if (state == 'still') {
+      $(this).attr('src', $(this).data('animate'));
+      console.log(this)
+      $(this).attr('data-state', 'animate');
+    }
+
+    else {
+      $(this).attr('src', $(this).data('still'));
+      $(this).attr('data-state', 'still');
+    }
+
+
+  })
+
+  animalButtons();
+});
